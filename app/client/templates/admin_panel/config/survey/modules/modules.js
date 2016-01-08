21b9958391f474d656d326e4e2f093249b1cd847
sniperx21x <laurent_0306@hotmail.com>
2016-01-08 13:14:24 +0100

    modules images

--- app/client/templates/admin_panel/config/survey/modules/modules.js
+++ app/client/templates/admin_panel/config/survey/modules/modules.js
@@ -1,3 +1,5 @@
+Session.set('nbrImg', 1);
+
 /* Give some default values */
 Template.ConfigModules.helpers ({
     surveyDefault : function(){
@@ -6,6 +8,26 @@ Template.ConfigModules.helpers ({
     surveyToAdd : function(){
         var survey = JSON.parse(sessionStorage.getItem("surveyToAdd"));
         return survey;
+    },
+    //retrieve the images objects and init reactively the counter of images
+    images : function() {
+        var nbrImg = Session.get('nbrImg');
+        var tabImg = [];
+        for(var i = 0; i < nbrImg; i++) {
+            var img = {};
+            img.order = i+1;
+            tabImg.push(img);
+        }
+        return tabImg;
+    }
+});
+
+/* Adding img modules template */
+Template.ConfigModules.events ({
+    'click #addImgLabel' : function (event) {
+        var nbrImg = Session.get('nbrImg');
+        nbrImg++;
+        Session.set('nbrImg', nbrImg);
     }
 });
 
@@ -15,4 +37,33 @@ Template.InputConfig.helpers ({
     isRadio : function(element){
         return element == "radio";
     }
+});
+
+/* some utils functions for config of filters in module img */
+Template.ContentModuleImg.helpers ({
+    //return every filters for the current module
+    filterAdmin : function (module) {
+        var surveyConfig = Meteor.settings.public.admin_panel.survey[0];
+        var currentModule;
+        $.each(surveyConfig.module_survey, function( index, modCurrent ) {
+            if(modCurrent.title == module) {
+                currentModule = modCurrent;
+                return false;
+            }
+        });
+        return currentModule.filter_admin;
+    }
+});
+
+/* events in Configuration for modules img => toggle an accordion */
+Template.ImgAdmin.events ({
+    'click .accordion' : function (event) {
+        $(event.target.parentNode).next().toggle("slow");
+    }
+});
+
+/* onrendered the ImgAdmin Template => toggle every modules and filter config */
+Template.ImgAdmin.onRendered (function () {
+    $(".contentAccordions").toggle();
+    $(".contentAccordionsFilter").toggle();
 });
