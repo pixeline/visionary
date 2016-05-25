dce12db85d79502cf192a7d0d6e42f83da37c6ec
teddy-k <teddy.tdk@gmail.com>
2016-05-25 23:13:24 +0200

    fatfree structure

---
+++ dev/admin/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Hello world</title>
+	</head>
+	<body>
+		Hello Admin
+		<script>
+			
+		</script>
+	</body>
+</html>
--- dev/index.php
+++ dev/index.php
@@ -8,77 +8,33 @@ if ((float)PCRE_VERSION<7.9) {
 	trigger_error('PCRE version is out of date');
 }
 
-// Load configuration
-$f3->config('config.ini');
+/* DATABASE CONFIG */
+if ($_SERVER['SERVER_NAME'] === 'localhost') {
+    // Development
+    $db_host		= 'localhost';
+	$db_user		= 'root';
+	$db_pass		= 'root';
+	$db_database	= 'visionary';
+	$db_port	    = '3306';
+} else {
+    // Development
+    $db_host		= 'localhost';
+	$db_user		= 'root';
+	$db_pass		= 'root';
+	$db_database	= 'visionary';
+	$db_port	    = '3306';
+}
 
-$f3->route('GET /',
-	function($f3) {
-		$classes=array(
-			'Base'=>
-			array(
-				'hash',
-				'json',
-				'session'
-			),
-			'Cache'=>
-			array(
-				'apc',
-				'memcache',
-				'wincache',
-				'xcache'
-			),
-			'DB\SQL'=>
-			array(
-				'pdo',
-				'pdo_dblib',
-				'pdo_mssql',
-				'pdo_mysql',
-				'pdo_odbc',
-				'pdo_pgsql',
-				'pdo_sqlite',
-				'pdo_sqlsrv'
-			),
-			'DB\Jig'=>
-			array('json'),
-			'DB\Mongo'=>
-			array(
-				'json',
-				'mongo'
-			),
-			'Auth'=>
-			array('ldap', 'pdo'),
-			'Bcrypt'=>
-			array(
-				'mcrypt',
-				'openssl'
-			),
-			'Image'=>
-			array('gd'),
-			'Lexicon'=>
-			array('iconv'),
-			'SMTP'=>
-			array('openssl'),
-			'Web'=>
-			array('curl', 'openssl', 'simplexml'),
-			'Web\Geo'=>
-			array('geoip', 'json'),
-			'Web\OpenID'=>
-			array('json', 'simplexml'),
-			'Web\Pingback'=>
-			array('dom', 'xmlrpc')
-		);
-		$f3->set('classes', $classes);
-		$f3->set('content', 'welcome.htm');
-		echo View::instance()->render('layout.htm');
-	}
+$options = array(
+    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION, // generic attribute
+    \PDO::ATTR_PERSISTENT => TRUE,  // we want to use persistent connections
+    \PDO::MYSQL_ATTR_COMPRESS => TRUE, // MySQL-specific attribute
 );
 
-$f3->route('GET /test',
-	function() {
-		echo 'Hello, world!';
-	}
-);
+$db = new \DB\SQL('mysql:host='.$db_host.';port='.$db_port.';dbname='.$db_database, $db_user, $db_pass, $options);
 
+
+/* HELPER */
 $f3->route('GET /userref',
 	function($f3) {
 		$f3->set('content', 'userref.htm');
@@ -87,11 +43,25 @@ $f3->route('GET /userref',
 );
 
 
+
+// Load configuration
+$f3->config('config.ini');
+
+$f3->route('GET /',function($f3) {
+	$f3->set('content', 'welcome.htm');
+	echo View::instance()->render('layout.htm');
+});
+/*
+$f3->route('GET /test',
+	function() {
+		echo 'Hello, world!';
+	}
+);
+*/
 /*
 	register, for informations: name, email and birth date
 	nince to have : connexion with auth facebook & auth google
 */
-$f3->route('GET /',function(){ });
 /*
 	create a unique url
 	save information into db 
@@ -105,16 +75,16 @@ $f3->route('GET /test',function(){ });
 	send back to '/'' if not good url
 	check if vetted
 */	
-$f3->route('GET /test/@unique_test_url',function(){ });  
-
+$f3->route('GET /test/@unique_test_url',function($f3){ 
+	echo 'Hello, world!';
+	var_dump($f3);
+});  
 /* 
 	results page 
 	url : /test/results
 	button try again that send back to 
 	if anonymous user ask for informations
 */
-
-
 // check admin login form
 $f3->route('POST /admin',function($f3){ });
 // Admin dashboard
@@ -126,6 +96,6 @@ $f3->route('GET /admin/users',function(){ });
 // charts
 $f3->route('GET /admin/analytics',function(){ });
 
-// [TODO] admin commands
+// [TODO] admin
 
 $f3->run();
